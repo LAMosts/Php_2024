7f69a9b26ac26c4d18ee510e6fd095818a8e841d
ML <lolopechka9@gmail.com>
2024-03-08 10:36:42 +0100

    Getter & setter user

--- classes/Users.php
+++ classes/Users.php
@@ -2,5 +2,42 @@
 require_once __DIR__ . "/Table.php";
 
 Class Users extends Table { 
-    
+    private $name;
+    private $email;
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     */
+    public function setName($name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
 }
